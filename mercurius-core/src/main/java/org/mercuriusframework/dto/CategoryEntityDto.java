90beeef668836da93231cfe316bd74ab251091ae
impi <ichigomaniac@gmail.com>
2017-02-21 21:42:47 +0300

    Added category url building

--- mercurius-core/src/main/java/org/mercuriusframework/dto/CategoryEntityDto.java
+++ mercurius-core/src/main/java/org/mercuriusframework/dto/CategoryEntityDto.java
@@ -4,4 +4,24 @@ package org.mercuriusframework.dto;
  * Category data transfer object
  */
 public class CategoryEntityDto extends CatalogUniqueCodeEntityDto {
+    /**
+     * Built url by bread crumbs
+     */
+    private String builtUrl;
+
+    /**
+     * Get built url
+     * @return Built url
+     */
+    public String getBuiltUrl() {
+        return builtUrl;
+    }
+
+    /**
+     * Set built url
+     * @param builtUrl Built url
+     */
+    public void setBuiltUrl(String builtUrl) {
+        this.builtUrl = builtUrl;
+    }
 }
--- mercurius-core/src/main/java/org/mercuriusframework/fillers/impl/CategoryEntityFiller.java
+++ mercurius-core/src/main/java/org/mercuriusframework/fillers/impl/CategoryEntityFiller.java
@@ -2,13 +2,24 @@ package org.mercuriusframework.fillers.impl;
 
 import org.mercuriusframework.dto.CategoryEntityDto;
 import org.mercuriusframework.entities.CategoryEntity;
+import org.mercuriusframework.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Category entity filler
  */
 @Service("categoryEntityFiller")
 public class CategoryEntityFiller extends CatalogUniqueCodeEntityFiller<CategoryEntity, CategoryEntityDto> {
+
+    /**
+     * Category service
+     */
+    @Autowired
+    private CategoryService categoryService;
+
     /**
      * Fill a result object from a source object
      * @param categoryEntity    Source object
@@ -17,5 +28,12 @@ public class CategoryEntityFiller extends CatalogUniqueCodeEntityFiller<Category
     @Override
     public void fillIn(CategoryEntity categoryEntity, CategoryEntityDto categoryEntityDto) {
         super.fillIn(categoryEntity, categoryEntityDto);
+        /** Build url */
+        List<CategoryEntity> breadcrumbs = categoryService.getBreadCrumbs(categoryEntity.getUuid());
+        StringBuilder buildUrl = new StringBuilder();
+        for (CategoryEntity temp : breadcrumbs) {
+            buildUrl.append("/" + temp.getCode());
+        }
+        categoryEntityDto.setBuiltUrl(buildUrl.toString());
     }
 }
